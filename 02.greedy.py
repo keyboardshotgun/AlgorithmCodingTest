5e3a0c31e1438d8997ce0fc14c7ef63c2be6f58f
keyboardshotgun <interse16@gmail.com>
2022-03-08 15:51:26 +0900

    설명 추가 +

--- 02.greedy.py
+++ 02.greedy.py
@@ -16,10 +16,11 @@ from random import randint
 # 여기서 힌트를 얻어 K+1 마다 반복 되므로, 나누어 떨어지는 특성을 이용하기로 하였다.
 # 즉, 전체반복 중 두번 째 큰수가 나오는 경우만 판단 하여 처리 하기로 하였다.
 
-K = 1
 M = 10
-array_length = 1000  # 배열의 크기 [2~1000]
-max_value = 10000  # 배열 각 항목의 크기 [1~10000]
+K = 2
+
+array_length = 10  # 배열의 크기 [2~1000]
+max_value = 10  # 배열 각 항목의 크기 [1~10000]
 numbers_array = []
 total = 0  # 총합
 total_text = ''  # 테스트용
@@ -44,14 +45,21 @@ for index in range(1, M + 1):
 print('my_total => ', total)
 
 # 검증
-count = 0
+# K = 3, M = 12
 
-# 가장 큰 수가 더해지는 횟수 계산
+# OOOX
+# OOOX
+# OOOX
+
+#  전체   /  가로   =  세로길이  * 가로일부(K)  = 큰수등장 횟수
+#    12  /  K+1   =       3  *         3 = 9
+#  (K+1) *  X     =      12 , 4X = 12, X = 3
+#   전체 - 큰수횟수 = 작은횟수
+#   12   - 9개    = 3개
+
 count = int(M / (K + 1)) * K
 count += M % (K + 1)
-
 result = 0
 result += count * first  # 가장 큰 수 더하기
 result += (M - count) * second  # 두 번째로 큰 수 더하기
-
 print('his_total =>', result)  # 최종 답안 출력
